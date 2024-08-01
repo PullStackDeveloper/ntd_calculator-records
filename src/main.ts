51322c001680b7fb450d1c5697b92935d02a0572
Fernando Nunes <pullstackdeveloper@protonmail.com>
2024-08-01 14:30:51 -0300

    Update port

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,15 @@ import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 import { ConfigService } from '@nestjs/config';
 import { AuthInterceptor } from './common/interceptors/auth.interceptor';
-
+const port = process.env.PORT || 5000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   const httpService = app.get(HttpService);
   app.setGlobalPrefix('api');
-  app.enableVersioning();
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
   app.enableCors({
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
@@ -18,6 +20,6 @@ async function bootstrap() {
   });
   const configService = app.get(ConfigService);
   app.useGlobalInterceptors(new AuthInterceptor(httpService, configService));
-  await app.listen(5000);
+  await app.listen(port);
 }
 bootstrap();
--- src/record/record.controller.ts
+++ src/record/record.controller.ts
@@ -6,8 +6,8 @@ import {
   Param,
   Post,
   Query,
-  Req,
-} from '@nestjs/common';
+  Req, Version
+} from "@nestjs/common";
 import { RecordService } from './record.service';
 import { Record } from './record.entity';
 import { Request } from 'express';
@@ -18,6 +18,7 @@ export class RecordController {
   constructor(private readonly recordService: RecordService) {}
 
   @Post()
+  @Version('1')
   async create(
     @Body() createRecordDto: CreateRecordDto,
     @Req() req: Request,
@@ -26,11 +27,13 @@ export class RecordController {
   }
 
   @Get(':id')
+  @Version('1')
   async findOne(@Param('id') id: number, @Req() req: Request): Promise<Record> {
     return this.recordService.findOne(id, req['user'].id);
   }
 
   @Get()
+  @Version('1')
   async findAll(
     @Req() req: Request,
     @Query('page') page: number = 1,
@@ -40,6 +43,7 @@ export class RecordController {
   }
 
   @Delete(':id')
+  @Version('1')
   async remove(@Param('id') id: number, @Req() req: Request): Promise<void> {
     return this.recordService.remove(id, req['user'].id);
   }
